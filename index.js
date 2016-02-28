7fb97e91efb0ed247bd5d410bac7fbcf584dd9ce
perborgen <perhb@hotmail.com>
2016-02-28 09:40:15 +0100

    Switched from express to hapi

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+const port = 8080;
+const host = 'localhost';
+
+server.connection({
+	port: process.env.PORT || port,
+    host: process.env.HOSTNAME || host
+});
+
+server.route([
+    {
+        method: "GET",
+        path: "/",
+        handler: function(request, reply){
+        	reply('hello');
+        }
+    }
+]);
+
+server.start(function(){
+    console.log(`Server running at ${host} port ${port}`);
+});
+
+
+/*app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -16,5 +42,5 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
+*/
 
