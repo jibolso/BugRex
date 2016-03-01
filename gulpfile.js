30f2c78b233bbcc48ec06219ed63640796887d29
perborgen <perhb@hotmail.com>
2016-03-01 18:21:42 +0100

    add a heroku:production dummy task. deploy working (not with gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -26,4 +26,8 @@ gulp.task('build', function () {
   build();
 });
 
+gulp.task("heroku:production", function(){
+    console.log('hello'); // the task does not need to do anything.
+});
+
 gulp.task('default', ['build']);
