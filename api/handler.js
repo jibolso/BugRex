06b755460653b4be40a7fe5547c8df882e79f234
perborgen <perhb@hotmail.com>
2016-03-06 10:17:53 +0100

    connect to mongolab. deploy works

--- api/handler.js
+++ api/handler.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const config = require('../config');
+
+mongoose.connect(process.env.MONGOLAB || config.mongolab);
+
 const featuredExperts = function(request, reply){
 	reply('Heloooo we are experts');
 };
 
+var userSchema = new Schema({
+    email: String,
+    name: String,
+    profileImg: String,
+    raw: Object,
+    token: String,
+    completedChats: Array,
+    facebookId: Number,
+    uniqueId: String
+});
 
 module.exports = {
 	featuredExperts: featuredExperts
